a5d245dd92459260dd344b2ae29950fae17c5377
MarfredBarrera <116857578+MarfredBarrera@users.noreply.github.com>
2024-12-17 01:49:50 -0800

    Add files via upload

--- SSA_main.jl
+++ SSA_main.jl
@@ -1,12 +1,17 @@
 include("SSA_utils.jl")
 include("SSA_particle_filter.jl")
+include("SSA_dynamics.jl")
+include("SSA_kernels.jl")
+
+using Plots
 
 struct Params
     M :: Int64
     N :: Int64
     L :: Int64
-    n :: Int32
+    n :: Int64
 end
+
 struct Gaussians
     # state density with mean and variance
     μ :: Float64
@@ -16,6 +21,7 @@ struct Gaussians
     ω :: Float64
     v :: Float64
 end
+
 struct Limits
     Ulim :: Float64
     α :: Float32
@@ -33,24 +39,24 @@ struct Limits
     y2_lowerlim :: Float64
 end
 
-
 # intialize parameters
 M = 1000
 N = 5
 L = 3000
 n = 2
+T = 10
 
 # state density mean and variance
 μ = 7.5 
 Σ = 0.5
 
-# process noise ωₖ and scalar measurement noise vₖ
-ω = 0.5
-v = 0.5
+#  process noise variance ωₖ and scalar measurement noise variance vₖ
+ω = 0.3
+v = 0.3
 
 Ulim = 3
-α = 0.05
-ϵ = 0.15
+α = 0.10
+ϵ = 0.20
 δ = 0.001
 
 # state constraints
@@ -74,10 +80,48 @@ SSA_limits = Limits(Ulim, α, ϵ, δ,
     x2_upperlim, x2_lowerlim, 
     y2_upperlim, y2_lowerlim)
 
+
+
+# initialize dynamics
+Q = Matrix{Float64}(I, 2, 2)
+R = v
+dynamics = Model(f,h,u,Q,R)
+x_true = Array{Float64}(undef, n, T)
+
 # generate state density Xi according to Gaussian parameters
 (Ξ_gaussian, W_gaussian, V_gaussian) = init_gaussians()
 Ξ = gpu_generate_Xi(SSA_params.L, SSA_params.n)
 
-@benchmark CUDA.@sync candidate_state = state_selection_algorithm(Ξ,SSA_params,SSA_limits)
+# initialize particle filter
+likelihoods = Vector(fill(1,(L)))
+pf = Particle_Filter(dynamics, TimeUpdate, MeasurementUpdate!, Resampler, likelihoods, Array(Ξ))
+
+x_true[:,1] = [μ; -μ].+ sqrt(Σ)*randn(2)
+
+# run the state selection algorithm for the particle density
+CUDA.@sync candidate_state, u_star, candidate_index = state_selection_algorithm(Ξ,SSA_params,SSA_limits)
 
 
+### run bootstrap particle filter
+Ξ = Array(Ξ)
+w = Array(gpu_sample_gaussian_distribution(0, ω, (n,L,1)))
+w_true = sqrt(ω)*randn(2)
+
+# propagate particle density
+Xi_plus = pf.TimeUpdate(Ξ, dynamics, u_star,w)
+pf.particles = Xi_plus
+x_true[:,2] = dynamics.f(x_true[:,1], u_star, w_true)
+
+# take measurement
+y = dynamics.h(x_true[:,2], sqrt(v)*randn())
+
+# calculate likelihoods
+pf.MeasurementUpdate(pf,dynamics,y)
+
+pf.Resampler(pf)
+
+plot(Ξ[1,:],Ξ[2,:], seriestype=:scatter)
+plot!(pf.particles[1,:], pf.particles[2,:], seriestype=:scatter)
+plot!(x_true[1,:], x_true[2,:], seriestype=:scatter)
+xlims!(-15,15)
+ylims!(-17,7)
--- SSA_utils.jl
+++ SSA_utils.jl
@@ -6,6 +6,8 @@ using Random
 using LinearAlgebra
 using Distributions
 
+include("SSA_kernels.jl")
+
 # TODO: have all parameters (covariances, particles, etc...) set from JSON file
 
 # function: init_gaussians() = initialize particle cloud, process noise, 
@@ -28,13 +30,12 @@ function init_gaussians()
     return (Ξ_gaussian, W_gaussian, V_gaussian)
 end
 
-
 ####
 # function: gpu_generate_Xi
 # input: L = number of particles
 # output: Ξ₀ = Array of randomly sampled states, size: [2 x L]
 ###
-function gpu_generate_Xi(L :: Int64, n :: Int32)
+function gpu_generate_Xi(L :: Int64, n :: Int64)
     # Gaussian Density with mean vector μ_x0 and covariance matrix Σ_x0
     μ_x0 = CuArray([7.5,-7.5])
     Σ_x0 = (0.5*I)
@@ -64,33 +65,6 @@ function cpu_generate_Xi(L :: Int64)
     return Array(Ξ₀)
 end
 
-
-# function: xprime_kernel_function!
-# inputs: 
-#   state - [2 x L x N] state array
-#   T - propagate up to time T
-#   w - randomly generated noise
-#   u - input [L x N]
-#
-# objective: propagate state estimates according to example dynamics, control, and noise using GPU
-function xprime_kernel_function!(state, T, w, u)
-    index = (blockIdx().x - 1) * blockDim().x + threadIdx().x
-    stride = gridDim().x * blockDim().x
-        for t ∈ 1:T-1
-            for i = index:stride:length(state)
-                @inbounds u[i,t] = -0.05*state[1,i,t]*state[2,i,t]
-                @inbounds state[1,i,t+1] = 0.9*state[1,i,t] + 0.2*state[2,i,t] + w[1,i,t]
-                @inbounds state[2,i,t+1] = -0.15*state[1,i,t] + 0.9*state[2,i,t] + 0.05*state[1,i,t]*state[2,i,t] + u[i,t] + w[2,i,t]
-            end
-        end 
- 
-        for i = index:stride:length(state)
-            @inbounds u[i,T] = -0.05*state[1,i,T]*state[2,i,T]
-        end
-    return
-end
-
-
 # function: launch_xprime_kernel
 # inputs: 
 #   state - [2 x L x N] state array
@@ -110,30 +84,12 @@ function launch_xprime_kernel!(state, T, w, u)
 end
 
 
-## function: monte_carlo_sampling_kernel - kernel function that calculates M samples for one particle
+## function: launch_xk2prime_kernel! - compute M number x'' sequences for one particle
 # inputs: T - time steps, M - sample number, state - initial state density, i - iterator through L
 #  u - input, w2 - randomly generated noise
 #
 # output: updated state array 
-function monte_carlo_sampling_kernel!(T, M, Ξ, state, u, w2, i)
-    index = (blockIdx().x - 1) * blockDim().x + threadIdx().x
-    stride = gridDim().x * blockDim().x
-
-    for t ∈ 1:T-1
-        for j = index:stride:length(state)
-            @inbounds state[1,j,t+1] = 0.9*state[1,j,t] + 0.2*state[2,j,t] + w2[1,j,t]
-            @inbounds state[2,j,t+1] = -0.15*state[1,j,t] + 0.9*state[2,j,t] + 0.05*state[1,j,t]*state[2,j,t] + u[i,t] + w2[2,j,t]
-        end
-    end
-end  
-
-
-## function: xk2prime! - compute M number x'' sequences for one particle
-# inputs: T - time steps, M - sample number, state - initial state density, i - iterator through L
-#  u - input, w2 - randomly generated noise
-#
-# output: updated state array 
-function xk2prime!(SSA_params, Ξ, state, u, w2, i)
+function launch_xk2prime_kernel!(SSA_params, Ξ, state, u, w2, i)
 
     L = SSA_params.L
     M = SSA_params.M 
@@ -154,7 +110,7 @@ function xk2prime!(SSA_params, Ξ, state, u, w2, i)
     end
 end
 
-# function: launch_master_kernel
+# function: launch_master_kernel!
 # objective: launch kernel for cost and violation rate calculation
 function launch_master_kernel!(SSA_limits, T, M, state, u, cost, state_violation_rate, control_violation_rate, i)
     kernel = @cuda launch=false master_kernel!(SSA_limits, T,M,state,u,cost,state_violation_rate, control_violation_rate,i)
@@ -167,63 +123,12 @@ function launch_master_kernel!(SSA_limits, T, M, state, u, cost, state_violation
     end
 end
 
-# function: master_kernel!
-# objective: from M number of x'' trajectories, calculate cost and violation rates simultaneously
-function master_kernel!(SSA_limits, T,M,state,u,cost,state_violation_rate, control_violation_rate, i)
-    cost_kernel!(T,M,state,u,cost,i)
-    constraint_violation_kernel!(SSA_limits, T,M,state,u,state_violation_rate, control_violation_rate, i)
-    return
-end
 
-# function: cost_kernel! - calculates the cost of M sampled trajectories for one particle
-function cost_kernel!(T,M,state,u,cost,i)
-    index = (blockIdx().x - 1) * blockDim().x + threadIdx().x
-    stride = gridDim().x * blockDim().x
-
-    for j in index:stride:M
-        for t in 1:T
-            cost[i] += state[1,j,t]^2 + state[2,j,t]^2 + u[i,t]^2
-        end
-    end
-
-    return
-end
-
-# function: constraint_violation_kernel!
-# objective: calculate constraint violation rates
-function constraint_violation_kernel!(SSA_limits, T,M,state,u,state_violation_rate, control_violation_rate, i)
-    # unpack SSA_limits struct
-    Ulim = SSA_limits.Ulim
-    x1_upperlim = SSA_limits.x1_upperlim
-    x1_lowerlim = SSA_limits.x1_lowerlim
-    y1_upperlim = SSA_limits.y1_upperlim
-    y1_lowerlim = SSA_limits.y1_lowerlim
-    x2_upperlim = SSA_limits.x2_upperlim
-    x2_lowerlim = SSA_limits.x2_upperlim
-    y2_upperlim = SSA_limits.y2_lowerlim
-    y2_lowerlim = SSA_limits.y2_upperlim
-
-    index = (blockIdx().x - 1) * blockDim().x + threadIdx().x
-    stride = gridDim().x * blockDim().x
-
-    # compare each trajectory with input and state constraints
-    for j = index:stride:M
-        for t = 1:T
-            if(u[i,t] > Ulim)
-                control_violation_rate[i] += 1
-            end
-            if((state[1,j,t] > x1_lowerlim && state[1,j,t] < x1_upperlim) ||
-                (state[2,j,t] > y1_lowerlim && state[2,j,t] < y1_upperlim) ||
-                (state[1,j,t] > x2_lowerlim && state[1,j,t] < x2_upperlim) ||
-                (state[2,j,t] > y2_lowerlim && state[2,j,t] < y2_upperlim))
-                state_violation_rate[i] += 1
-            end
-        end
-    end
-    return
-end
-
-
+# function: state_selection_algorithm
+# inputs:
+# - Ξ: particle density
+# - SSA_params: struct of parameters
+# - SSA_limits: struct of state and input constraints
 function state_selection_algorithm(Ξ,SSA_params,SSA_limits)
     n = SSA_params.n
     # intialize state array
@@ -240,39 +145,38 @@ function state_selection_algorithm(Ξ,SSA_params,SSA_limits)
     w2 = ((gpu_sample_gaussian_distribution(0, ω, (n,L,N))))
 
     # ### First, lets generate the x' trajectories for time horizon N for each particle in state density Xi ###
-    launch_xprime_kernel!(state, N, w, u)
+    CUDA.@sync launch_xprime_kernel!(state, N, w, u)
 
 
-    # declare vectors for x'' trajectories, cost, and constrain rate calculations
+    # declare vectors for x'' trajectories, cost, and constraint violation rate calculations
     cost = CUDA.fill(0.0f0, L)
     state_violation_rate = CUDA.fill(0.0f0,L)
     control_violation_rate = CUDA.fill(0.0f0,L)
     state_2prime = CUDA.fill(0.0f0, (n,M,N))
 
-
     # iterate through each particle in Ξ and run M monte carlo simulations for each particle 
     for i = 1:L
-
+        
         # calculate x'' trajectories
-        xk2prime!(SSA_params, Ξ, state_2prime, u, w2, i)
+        CUDA.@sync launch_xk2prime_kernel!(SSA_params, Ξ, state_2prime, u, w2, i)
 
         # calculate cost and state/control violation rates
-        launch_master_kernel!(SSA_limits, N, M, state_2prime, u, cost, state_violation_rate, control_violation_rate, i)
+        CUDA.@sync launch_master_kernel!(SSA_limits, N, M, state_2prime, u, cost, state_violation_rate, control_violation_rate, i)
     end
 
     # sample-average violation rate counts
     state_violation_rate /= M
     control_violation_rate /= M
 
-    
     state_feasibility_mask = state_violation_rate .< α
     control_feasibility_mask = control_violation_rate .< α
 
     # mask for feasible states
     feasibility_mask = state_feasibility_mask .& control_feasibility_mask
 
+    u = Array(u)
+
     # find feasible state with minimum cost
     cost_val, candidate_index = findmin(cost[feasibility_mask])
-
-    return Array(Ξ)[:,candidate_index,1]
+    return Array(Ξ)[:,candidate_index,1], u[candidate_index,1], candidate_index
 end

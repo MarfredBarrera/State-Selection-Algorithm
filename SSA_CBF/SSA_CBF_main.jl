f3f537be9dc2ae95ad41707b80a0703453012bb8
Marfred Barrera <mrbarrera@ucsd.edu>
2025-02-06 09:41:40 -0800

    clean up

--- SSA_CBF/SSA_CBF_main.jl
+++ SSA_CBF/SSA_CBF_main.jl
@@ -15,7 +15,7 @@ function gpu_generate_Xi(L :: Int64, n :: Int64, μ::Vector{Float64}, var)
      # Gaussian Density with mean vector μ_x0 and covariance matrix Σ_x0
      μ_x0 = Array(μ)
      Σ_x0 = (var*I)
- 
+
      # randomly sample initial states Ξ following Gaussian density
      Ξ₀ = Array{Float64}(undef,n,L)
      Ξ₀ = μ_x0.+vcat(sqrt(Σ_x0)*randn(2,L), 0.0*randn(2,L))
@@ -33,12 +33,11 @@ end
 
 ## DEFINE SSA PARAMETERS ##
 # Density and sampling parameters
-L = 400 # number of particles
-M = 135  # number of Monte Carlo samples
-T = 10   # total simulation time [seconds]
+L = 300 # number of particles
+M = 150  # number of Monte Carlo samples
+T = 10  # total simulation time [seconds]
 K = Int64(T/Δt) # total simulation steps
-N = Int64(0.10*K)   # time horizon
-
+N = 5   # time horizon
 # intialize parameters
 SSA_params = Params(M, N, L, n, T, K)
 w = 0.05
@@ -48,7 +47,7 @@ r_goal = [0.0, 0.0]
 kd(x) = -Kp*(x[1:2]-r_goal)-Kd*x[3:4]
 
 # initalize a control affine system
-σ = Dynamics(f,g,H,fd,gd,kd,A,B,Q,R,n,m)
+Σ = Dynamics(f,g,H,fd,gd,kd,A,B,Q,R,n,m)
 
 # initialize state and control arrays
 state = Array{Float64}(undef, (n,K,L))
@@ -63,44 +62,67 @@ dmin = 1.25
 r0 = [1.6, 1.45]
 h(x) = norm(x[1:2]-r0)^2 - dmin^2
 α(r) = 1.0*r
-cbf = ControlBarrierFunction(h, σ, α)
+cbf = ControlBarrierFunction(h, Σ, α)
 J(x,u) = h(x)
 
 # Define safety filter with the cbf and nominal controller
-k_cbf = QPSafetyFilter(cbf, σ, kd)
+k_cbf = QPSafetyFilter(cbf, Σ, kd)
 
 # Define boostrap particle filter
-pf = Particle_Filter(σ, TimeUpdate, MeasurementUpdate, Resampler, ones(L), x0)
+pf = Particle_Filter(Σ, TimeUpdate, MeasurementUpdate, Resampler, ones(L), x0)
 xtrue = Array{Float64}(undef, (n,K))
 xtrue[:,1] = x0_true
-
 xstar = Array{Float64}(undef, (n,K))
 
-u_total = Array{Float64}(undef, (m,N,L))
 
-function simulate!(SSA_params, xstar, xtrue, state, Σ::Dynamics, sf::QPSafetyFilter, pf::Particle_Filter,J::Function)
+
+""" 
+function: simulate!
+     inputs:
+          SSA_params - struct of SSA parameters
+          xstar - [n x K] size array to track candidate state
+          xtrue - [n x K] size array to track true state
+          Σ - dynamics of a control affine system
+          sf - quadratic programming safety filter object
+          pf - particle filter object
+          J - prescibed cost function
+          RUN_SSA - flag for SSA or CM
+"""
+function simulate!(SSA_params, xstar, xtrue, state, 
+     Σ::Dynamics, sf::QPSafetyFilter, pf::Particle_Filter,J::Function, RUN_SSA)
      K = SSA_params.K
-     for t = 1:K-1
-          Ξ = state[:,t,:]
-          # x_star = vec(mean(Ξ, dims=2))
-          x_star = state_selection_algorithm(SSA_params, Ξ, x->sf(x), Σ, J)
-          u_star = sf(x_star)
 
+     for t = 1:K-1
+
+          # define starting particle density
+          Ξ = state[:,t,:]
+
+          # select state for certainty equivalence
+          if(RUN_SSA)
+               x_star = state_selection_algorithm(SSA_params, Ξ, x->sf(x), Σ, J)
+          else
+               x_star = vec(mean(Ξ, dims=2))
+          end
+
+          # define control signal
+          u_star = sf(x_star)
           xstar[:,t] = x_star
 
+          # propagate true state and particle filter
           xtrue[:,t+1] = dynamics(Σ, xtrue[:,t], u_star)
-
           y = Σ.H(xtrue[:,t+1])
-
           propagate_bootstrap_pf(pf,Σ,u_star,Ξ,y)
+
+          # update state
           state[:,t+1,:] = pf.particles
+
+
           println(t)
      end
 end
 
-simulate!(SSA_params, xstar, xtrue, state, σ, k_cbf, pf, J)
-
-
+run_ssa = true
+simulate!(SSA_params, xstar, xtrue, state, Σ, k_cbf, pf, J, run_ssa)
 
 
 
--- SSA_CBF/State_Selection_Algorithm.jl
+++ SSA_CBF/State_Selection_Algorithm.jl
@@ -1,5 +1,15 @@
 
 
+"""
+function: xprime!
+    objective: calculate open loop trajectories and control sequences for each particle
+    inputs:
+        u - [m x N x L] size array for storing control sequences for all particles
+        SSA_params - struct of SSA parameters
+        state - [n x N x L] size array for storing trajectories of all particles
+        Σ - dynamics for a control affine system
+        kd - control law for u = kd(x)
+"""
 function xprime!(u, SSA_params, state, Σ::Dynamics, kd::Function)
     Threads.@threads for i = 1:SSA_params.L
          for t = 1:SSA_params.N-1
@@ -9,14 +19,36 @@ function xprime!(u, SSA_params, state, Σ::Dynamics, kd::Function)
     end
 end
 
+
+"""
+function: x2prime!
+    objective: calculate trajectories for sampled particles given a control sequence from xprime
+    inputs:
+        u - [m x N] size array for the control sequence to be applied
+        SSA_params - struct of SSA parameters
+        state - [n x N x M] size array for storing trajectories of all sampled particles
+        Σ - dynamics for a control affine system
+        kd - control law for u = kd(x)
+"""
 function x2prime!(u, SSA_params, state, Σ::Dynamics, kd::Function)
-    for i = 1:SSA_params.M
+    for j = 1:SSA_params.M
          for t = 1:SSA_params.N-1
-              state[:,t+1,i] = dynamics(Σ, state[:,t,i], u[:,t])
+                state[:,t+1,j] = dynamics(Σ, state[:,t,j], u[:,t])
          end
     end
 end
 
+
+"""
+function: state_selection_algorithm
+    objective: select a single particle from a particle density according to SSA
+    inputs:
+        SSA_params - struct of SSA parameters
+        Ξ - [n x L] particle density
+        kd - control law for u = kd(x)
+        Σ - dynamics for a control affine system
+        J - prescibed cost function
+"""
 function state_selection_algorithm(SSA_params, Ξ, kd::Function, Σ::Dynamics, J::Function)
     n = SSA_params.n
     N = SSA_params.N
@@ -30,13 +62,15 @@ function state_selection_algorithm(SSA_params, Ξ, kd::Function, Σ::Dynamics, J
     state_2prime = Array{Float64}(undef,(n,N,M))
     cost = Array{Float64}(undef, L)
     total_state_violations = zeros(L)
-    sampled_state_violation_count = zeros(L)
+    sampled_state_violation_count = zeros(N,M)
     feasibility_mask = falses(L)
     
 
+    # calculate xprime sequence
     state[:,1,:] = Ξ
     xprime!(u_total, SSA_params, state, Σ, kd)
 
+    # for each particle, carry out monte carlo sampling
     Threads.@threads for i = 1:L
 
         if(check_constraints(state[:,1,i]) > 0)
@@ -45,23 +79,29 @@ function state_selection_algorithm(SSA_params, Ξ, kd::Function, Σ::Dynamics, J
         end
 
         mc_sample_index = (rand(1:L, M))
+
+        # randomly sample M particles
         state_2prime[:,1,:] = Ξ[:,mc_sample_index]
 
+        # extract control sequence for ith particle
         u_j = u_total[:,:,i]
+
+        # apply control sequence and calculate x2prime sequence
         x2prime!(u_j, SSA_params, state_2prime, Σ, kd)
 
+        # calculate cost of given batch of monte carlo samples
         for j = 1:M
             for t = 1:N
-                cost[i] += J(state_2prime[:,t,j], u_j[:,t]) + J(state[:,t,i],u_j[:,t])
-                # if(check_constraints(state_2prime[:,t,j]) == 1)
-                #     sampled_state_violation_count[j] = 1.0
-                # end
+                cost[i] += J(state_2prime[:,t,j], u_j[:,t]) + J(state[:,t,i], u_j[:,t])
+                if(check_constraints(state_2prime[:,t,j]) == 1)
+                    sampled_state_violation_count[t,j] = 1.0
+                end
             end
         end
-        # total_state_violations[i] = sum(sampled_state_violation_count)/M
-    end
 
-    feasibility_mask = total_state_violations .< α
+        # only include feasible states
+        feasibility_mask[i] = all(sum(sampled_state_violation_count,dims=2)/M .< α) 
+    end
 
 
     if(sum(feasibility_mask) <= 0)
